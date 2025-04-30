c4ae41a97805fb644be8aa47e93141cb2678a06b
rishabhraj36 <rshvraj36@gmail.com>
2025-04-30 09:54:36 +0530

    feat: Initial commit from MCP CLI init command

---
+++ src/universal_mcp_wrike/mcp.py
@@ -0,0 +1,19 @@
+
+from universal_mcp.servers.server import SingleMCPServer
+from universal_mcp.integrations.agentr import AgentRIntegration
+from universal_mcp.stores.store import EnvironmentStore
+
+from universal_mcp_wrike.app import WrikeApp
+
+env_store = EnvironmentStore()
+integration_instance = AgentRIntegration(name="wrike", store=env_store)
+app_instance = WrikeApp(integration=integration_instance)
+
+mcp = SingleMCPServer(
+    app_instance=app_instance,
+)
+
+if __name__ == "__main__":
+    mcp.run()
+
+
